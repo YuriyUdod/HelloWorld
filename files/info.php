b7228bf8b551e732dda5212365737adea46c8023
YuriyUdod <127054312+YuriyUdod@users.noreply.github.com>
2023-03-18 02:43:02 +0100

    Update info.php

--- files/info.php
+++ files/info.php
@@ -1,2 +1,5 @@
 <?php
-phpinfo();
+$ip = $_SERVER['REMOTE_ADDR'];
+echo "IP-адреса: $ip";
+?>
+<img src="http://www.rtdesigngroup.com/wp-content/uploads/2014/04/php-programming.jpg" alt="PHP Programming">
